ad1a57e57147ed9554922abe37a393bb6d5a3c3f
Gidi Babz <tunex4ind@gmail.com>
2022-06-28 16:52:21 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-// import { authActions } from '../store/index';
+
 import Counter from './components/Counter';
 import Header from './components/Header';
 import Auth from './components/Auth';
@@ -8,6 +8,7 @@ import UserProfile from './components/UserProfile';
 
 function App() {
 	const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
 	return (
 		<Fragment>
 			<Header />
